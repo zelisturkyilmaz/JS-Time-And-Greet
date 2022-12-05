1073812b3bdd42bbb6b292d027f15f0b8e157459
Zelis Turkyilmaz <zelis.turkyilmaz1@gmail.com>
2022-12-05 18:37:10 +0300

    Add time and day

--- js/clock.js
+++ js/clock.js
@@ -2,3 +2,22 @@ let userName = prompt("What's your name?");
 
 document.getElementById("myName").innerHTML = userName;
 
+function showTime() {
+    const date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    const weekdays = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+
+    let day = weekdays[date.getDay()];
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    let time = `${hour} : ${minute} : ${seconds} ${day}`;
+    document.getElementById("myClock").innerHTML = time;
+}
+
+setInterval(showTime, 1000);
